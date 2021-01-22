3c70d248ae16098307c8fad43d566b84a1a1bacf
Nick Catalano <catalano936@gmail.com>
2021-01-22 02:26:46 -0500

    AddedTank

---
+++ Assets/Scripts/tank_controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Tank
+{
+       public class tank_controller: MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+           
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        
+    }
+}
---
+++ Assets/Scripts/tank_inputs.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Tank
+{
+    public class tank_inputs : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
