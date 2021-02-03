41b6ddc35ad0c1b0c5a927ca6b2efb27609410d5
Kevin Patel <kevinptl4@gmail.com>
2021-02-02 20:48:32 -0500

    adding sphere collider movement functionality

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -10,38 +10,20 @@ namespace SBC
         public float tankSpeed;
         public float rotateSpeed;
         public float brakeForce;
-    
-        public List<WheelCollider> movementWheels;
-        public List<WheelCollider> rotateWheels;
+        public Rigidbody rb;
 
         void FixedUpdate()
         {
             float forwards = Input.GetAxis("Vertical");
             float rotate = Input.GetAxis("Horizontal");
             bool brake = Input.GetKey(KeyCode.Space);
-            foreach (WheelCollider wheel in movementWheels)
-            {
-                wheel.motorTorque = tankSpeed * forwards * Time.deltaTime;
-            }
+            //Vector3 moveTank = transform.position + (transform.forward * forwards * tankSpeed * Time.deltaTime);
+            Vector3 moveTank = new Vector3(0f, 0f, forwards * tankSpeed * Time.deltaTime);
+            //Quaternion rotateTank = transform.rotation * Quaternion.Euler(Vector3.up * (rotateSpeed * rotate * Time.deltaTime));
+            // rb.MovePosition(moveTank);
 
-            foreach (WheelCollider wheel in rotateWheels)
-            {
-                wheel.steerAngle = rotateSpeed * rotate * Time.deltaTime;
-            }
-
-            if (brake) 
-            {
-                foreach (WheelCollider wheel in movementWheels)
-                {
-                    wheel.brakeTorque = brakeForce;
-                }
-            } else 
-            {
-                foreach (WheelCollider wheel in movementWheels)
-                {
-                    wheel.brakeTorque = 0f;
-                }
-            }
+            rb.AddRelativeForce(moveTank);
+            rb.AddRelativeTorque(rotate * Vector3.up * rotateSpeed);
 
         }
     }
