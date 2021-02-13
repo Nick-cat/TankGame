d3af87ea4a6e963a9b130dc8a9be4e56116c8f4c
Kevin Patel <kevinptl4@gmail.com>
2021-02-13 17:00:13 -0500

    modifying the movement code to detect ground, move with more drag, and move only on ground and not in air

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -10,21 +10,43 @@ namespace SBC
         public float tankSpeed;
         public float rotateSpeed;
         public float brakeForce;
+        public float maxVelocity;
         public Rigidbody rb;
-
+        public float distanceToGround = .5f;
+        public LayerMask ground;
+        public Transform rayPoint;
         void FixedUpdate()
         {
             float forwards = Input.GetAxis("Vertical");
             float rotate = Input.GetAxis("Horizontal");
             bool brake = Input.GetKey(KeyCode.Space);
-            //Vector3 moveTank = transform.position + (transform.forward * forwards * tankSpeed * Time.deltaTime);
+            
             Vector3 moveTank = new Vector3(0f, 0f, forwards * tankSpeed * Time.deltaTime);
-            //Quaternion rotateTank = transform.rotation * Quaternion.Euler(Vector3.up * (rotateSpeed * rotate * Time.deltaTime));
-            // rb.MovePosition(moveTank);
+            if (isGrounded()) {
+                if (rb.velocity.sqrMagnitude < maxVelocity)
+                {
+                    rb.AddForce(transform.forward * forwards * tankSpeed * 100f);
+                    Vector3 rotateAmount = new Vector3(0f, rotate * rotateSpeed * Time.deltaTime, 0f);
+                    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + rotateAmount);
+                }
+            }
 
-            rb.AddRelativeForce(moveTank);
-            rb.AddRelativeTorque(rotate * Vector3.up * rotateSpeed);
+            rb.AddForce(-transform.up * 100f);
+            if (brake) 
+            {
+                rb.angularDrag = 1.5f;
+                rb.drag = 3f;
+            } else {
+                rb.angularDrag = 0;
+                rb.drag = 0;
+            }
+        }
 
+        bool isGrounded(){
+            //cast a ray from center of body to down direction, check if anything intersects the bottom of the body.
+            RaycastHit hit;
+            bool val = Physics.Raycast(rayPoint.position, -transform.up, out hit, distanceToGround, ground);
+            return val;
         }
     }
 }
