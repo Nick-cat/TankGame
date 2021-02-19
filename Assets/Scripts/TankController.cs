178ce7a3897b56a409abf5ae892cf468d0dd0eb3
Nick Catalano <catalano936@gmail.com>
2021-02-19 02:05:50 -0500

    Adjusted Tank Controller script
    now uses torque for tank rotation
    can now change centre of mass, to avoid getting stuck upside down

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -11,6 +11,7 @@ namespace SBC
         public float rotateSpeed;
         public float brakeForce;
         public float maxVelocity;
+        public Vector3 centerOfMass;
         private Rigidbody rb;
         public float distanceToGround = .5f;
         public LayerMask ground;
@@ -19,24 +20,28 @@ namespace SBC
         private void Start()
         {
             rb = GetComponent <Rigidbody>();
+            rb.centerOfMass = centerOfMass;
         }
         void FixedUpdate()
         {
-            float forwards = Input.GetAxis("Vertical");
-            float rotate = Input.GetAxis("Horizontal");
-            bool brake = Input.GetKey(KeyCode.Space);
+           float forwards = Input.GetAxis("Vertical");
+           float rotate = Input.GetAxis("Horizontal");
+           bool brake = Input.GetKey(KeyCode.Space);
             
-            Vector3 moveTank = new Vector3(0f, 0f, forwards * tankSpeed * Time.deltaTime);
-            if (isGrounded()) {
+           Vector3 moveTank = new Vector3(0f, 0f, forwards * tankSpeed * Time.deltaTime);
+           if (isGrounded()) {
                 if (rb.velocity.sqrMagnitude < maxVelocity)
                 {
                     rb.AddForce(transform.forward * forwards * tankSpeed * 100f);
-                    Vector3 rotateAmount = new Vector3(0f, rotate * rotateSpeed * Time.deltaTime, 0f);
-                    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + rotateAmount);
+
+                //Vector3 rotateAmount = new Vector3(0f, rotate * rotateSpeed * Time.deltaTime, 0f);
+                //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + rotateAmount);
                 }
             }
+            
+            rb.AddTorque(transform.up * rotateSpeed * rotate);
 
-            rb.AddForce(-transform.up * 100f);
+            //rb.AddForce(-transform.up * 100f);
             if (brake) 
             {
                 rb.angularDrag = 1.5f;
@@ -48,9 +53,9 @@ namespace SBC
         }
         bool isGrounded(){
             //cast a ray from center of body to down direction, check if anything intersects the bottom of the body.
-            RaycastHit hit;
-            bool val = Physics.Raycast(rayPoint.position, -transform.up, out hit, distanceToGround, ground);
-           return val;
-        }
+            //RaycastHit hit;
+            bool val = Physics.Raycast(rayPoint.position, -transform.up, distanceToGround, ground);
+            return val;
+       }
     }
 }
