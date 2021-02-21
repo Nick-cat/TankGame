1d73a93906a8778e07b8bbfb11e1ff889b33b6b4
Nick Catalano <catalano936@gmail.com>
2021-02-21 18:23:37 -0500

    added quality, resolution, and fullscreen settings to menu

--- Assets/Scripts/MenuScripts/OptionsMenu.cs
+++ Assets/Scripts/MenuScripts/OptionsMenu.cs
@@ -2,25 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width &&
+                resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     public void SetMainVolume(float mainVolume)
     {
-        audioMixer.SetFloat("mainVolume", mainVolume);
+        audioMixer.SetFloat("mainVolume", Mathf.Log10(mainVolume) * 20);
     }
     public void SetMusicVolume(float musicVolume)
     {
-        audioMixer.SetFloat("musicVolume", musicVolume);
+        audioMixer.SetFloat("musicVolume", Mathf.Log10(musicVolume) * 20);
     }
     public void SetEffectsVolume(float effectsVolume)
     {
-        audioMixer.SetFloat("effectsVolume", effectsVolume);
+        audioMixer.SetFloat("effectsVolume", Mathf.Log10(effectsVolume) * 20);
     }
     public void SetQualityLevel(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void ToggleFullscreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+
+        Debug.Log(Screen.fullScreenMode);
+
     }
 
 
