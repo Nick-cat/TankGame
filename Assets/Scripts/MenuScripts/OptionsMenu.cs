86826743a9db83a5d3b38681040f86c9d1d0abb1
Nick Catalano <catalano936@gmail.com>
2021-02-25 17:11:50 -0500

    Big Audio Updates
    added second song
    added shit tank engine sound that needs work
    added music player script

--- Assets/Scripts/MenuScripts/OptionsMenu.cs
+++ Assets/Scripts/MenuScripts/OptionsMenu.cs
@@ -4,65 +4,68 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class OptionsMenu : MonoBehaviour
+namespace SBC
 {
-    public AudioMixer audioMixer;
-    public TMPro.TMP_Dropdown resolutionDropdown;
-
-    Resolution[] resolutions;
-
-    private void Start()
+    public class OptionsMenu : MonoBehaviour
     {
+        public AudioMixer audioMixer;
+        public TMPro.TMP_Dropdown resolutionDropdown;
 
-        //Resolution dropdown setup
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
+        Resolution[] resolutions;
 
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
+        private void Start()
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
 
-            if (resolutions[i].width == Screen.width &&
-                resolutions[i].height == Screen.height)
+            //Resolution dropdown setup
+            resolutions = Screen.resolutions;
+            resolutionDropdown.ClearOptions();
+
+            List<string> options = new List<string>();
+
+            int currentResolutionIndex = 0;
+            for (int i = 0; i < resolutions.Length; i++)
             {
-                currentResolutionIndex = i;
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                options.Add(option);
+
+                if (resolutions[i].width == Screen.width &&
+                    resolutions[i].height == Screen.height)
+                {
+                    currentResolutionIndex = i;
+                }
             }
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
         }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
 
-    public void SetResolution(int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
+        public void SetResolution(int resolutionIndex)
+        {
+            Resolution resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
 
-    public void SetMainVolume(float volume)
-    {
-        audioMixer.SetFloat("mainVolume", Mathf.Log10(volume) * 20);
-    }
-    public void SetMusicVolume(float volume)
-    {
-        audioMixer.SetFloat("musicVolume", Mathf.Log10(volume) * 20);
-    }
-    public void SetEffectsVolume(float volume)
-    {
-        audioMixer.SetFloat("effectsVolume", Mathf.Log10(volume) * 20);
-    }
-    public void SetQualityLevel(int qualityIndex)
-    {
-        QualitySettings.SetQualityLevel(qualityIndex);
-    }
+        public void SetMainVolume(float volume)
+        {
+            audioMixer.SetFloat("mainVolume", Mathf.Log10(volume) * 20);
+        }
+        public void SetMusicVolume(float volume)
+        {
+            audioMixer.SetFloat("musicVolume", Mathf.Log10(volume) * 20);
+        }
+        public void SetEffectsVolume(float volume)
+        {
+            audioMixer.SetFloat("effectsVolume", Mathf.Log10(volume) * 20);
+        }
+        public void SetQualityLevel(int qualityIndex)
+        {
+            QualitySettings.SetQualityLevel(qualityIndex);
+        }
 
-    public void ToggleFullscreen(bool fullscreen)
-    {
-        Screen.fullScreen = fullscreen;
-        Debug.Log(Screen.fullScreenMode);
+        public void ToggleFullscreen(bool fullscreen)
+        {
+            Screen.fullScreen = fullscreen;
+            Debug.Log(Screen.fullScreenMode);
+        }
     }
 }
--- Assets/Scripts/MenuScripts/TankSelection.cs
+++ Assets/Scripts/MenuScripts/TankSelection.cs
@@ -3,63 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TankSelection : MonoBehaviour
+namespace SBC
 {
-
-    private GameObject[] tankList;
-    private int tankIndex;
-    public LevelSelectManager levelSelect;
-
-    void Start()
+    public class TankSelection : MonoBehaviour
     {
-        tankList = new GameObject[transform.childCount];
 
-        //fill tank array
-        for (int i = 0; i < transform.childCount; i++) 
-            tankList[i] = transform.GetChild(i).gameObject;
+        private GameObject[] tankList;
+        private int tankIndex;
+        public LevelSelectManager levelSelect;
 
-        //toggle off tank renderers
-        foreach(GameObject go in tankList) go.SetActive(false);
+        void Start()
+        {
+            tankList = new GameObject[transform.childCount];
 
-        //toggle on first 
-        if (tankList[0])
-            tankList[0].SetActive(true);
+            //fill tank array
+            for (int i = 0; i < transform.childCount; i++)
+                tankList[i] = transform.GetChild(i).gameObject;
+
+            //toggle off tank renderers
+            foreach (GameObject go in tankList) go.SetActive(false);
+
+            //toggle on first 
+            if (tankList[0])
+                tankList[0].SetActive(true);
+        }
+
+        public void ToggleLeft()
+        {
+            //toggle off current model
+            tankList[tankIndex].SetActive(false);
+
+            tankIndex--;
+            if (tankIndex < 0)
+                tankIndex = tankList.Length - 1;
+
+            //toggle on new model
+            tankList[tankIndex].SetActive(true);
+        }
+
+        public void ToggleRight()
+        {
+            //toggle off current model
+            tankList[tankIndex].SetActive(false);
+
+            tankIndex++;
+            if (tankIndex == tankList.Length)
+                tankIndex = 0;
+
+            //toggle on new model
+            tankList[tankIndex].SetActive(true);
+        }
+
+        public void Confirm()
+        {
+            PlayerPrefs.SetInt("Tank", tankIndex);
+            if (PlayerPrefs.HasKey("Tank"))
+                Debug.Log(PlayerPrefs.GetInt("Tank"));
+            //Debug.Log(levelSelect.level);
+            SceneManager.LoadScene(levelSelect.level);
+
+        }
+
     }
-
-    public void ToggleLeft()
-    {
-        //toggle off current model
-        tankList[tankIndex].SetActive(false);
-        
-        tankIndex--;
-        if (tankIndex < 0)
-            tankIndex = tankList.Length - 1;
-
-        //toggle on new model
-        tankList[tankIndex].SetActive(true);
-    }
-
-    public void ToggleRight()
-    {
-        //toggle off current model
-        tankList[tankIndex].SetActive(false);
-
-        tankIndex++;
-        if (tankIndex == tankList.Length)
-            tankIndex = 0;
-
-        //toggle on new model
-        tankList[tankIndex].SetActive(true);
-    }
-
-    public void Confirm()
-    {
-        PlayerPrefs.SetInt("Tank", tankIndex);
-        if (PlayerPrefs.HasKey("Tank"))
-        Debug.Log(PlayerPrefs.GetInt("Tank"));
-        //Debug.Log(levelSelect.level);
-        SceneManager.LoadScene(levelSelect.level);
-
-    }
-
 }
