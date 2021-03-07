573523e6eadba33fc9c618971a1827398cc14667
Nick Catalano <catalano936@gmail.com>
2021-03-07 12:46:39 -0500

    hologram shader update

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+    private GameObject player;
+    public bool lookAtPlayer;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+    private void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     void LateUpdate()
     {
-        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
-        Vector3 eulerAngles = transform.eulerAngles;
-        eulerAngles.x = 0;
-        transform.eulerAngles = eulerAngles;
+        if (lookAtPlayer)
+        {
+            transform.LookAt(player.transform, Vector3.up);
+            Vector3 eulerAngles = transform.eulerAngles;
+            eulerAngles.x = 0;
+            transform.eulerAngles = eulerAngles;
+
+        }
+        else
+        {
+            transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+            Vector3 eulerAngles = transform.eulerAngles;
+            eulerAngles.x = 0;
+            transform.eulerAngles = eulerAngles;
+        }
     }
 }
