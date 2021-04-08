1ebb26ab56ea0b25ec1ce2758bd8c3ef2aecd0db
Nick Catalano <catalano936@gmail.com>
2021-04-07 23:11:39 -0400

    Added basic bouncy ball ammo

---
+++ Assets/Scripts/BounceAmmo.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SBC
+{
+    public class BounceAmmo : TankRound
+    {
+        protected AmmoType ammotype = AmmoType.BouncyBall;
+
+        [SerializeField] private GameObject explosionEffectObject;
+        public float explosionRadius = 2;
+        public float explosionForce = 100;
+        [SerializeField] int bouncesUntilDestroy = 3;
+
+        public override void OnHit()
+        {
+            // Explosion force.
+            Vector3 pos = transform.position;
+            Collider[] cols = Physics.OverlapSphere(pos, explosionRadius);
+            foreach (Collider c in cols)
+            {
+                Rigidbody rb = c.attachedRigidbody;
+                if (rb != null) rb.AddExplosionForce(explosionForce, pos, explosionRadius, 1.0f);
+
+                //deal damage
+                if (c.gameObject.CompareTag("Enemy"))
+                {
+                    c.gameObject.GetComponent<HealthManager>().Hurt(explosionForce);
+                }
+            }
+            bouncesUntilDestroy -= 1;
+
+            if (explosionEffectObject != null) Instantiate(explosionEffectObject, transform.position, Quaternion.identity);
+            if(bouncesUntilDestroy == 0) Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SimpleGravity.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleGravity : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float customGravity = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(Vector3.up * -customGravity * 100f);
+    }
+}
